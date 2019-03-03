388ee0837214da04e28644456142eba32ef588ef
Willy Ngoupoyou <touomiwilly@gmail.com>
2019-03-03 12:05:04 -0500

    Changing port exposed

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ COPY ./ ./
 RUN npm run build
 
 # Phase II : Prod phase
-FROM nginx as prod
+FROM nginx
 #EXPOSE 80
+ENV PORT 8080
 COPY --from=builder /app/build /usr/share/nginx/html
---
+++ _build/precache-manifest.7f4580dd12ed5c15650df806c27e14d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "79e668ef6d55bc788ec6",
+    "url": "/static/js/main.79e668ef.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "79e668ef6d55bc788ec6",
+    "url": "/static/css/main.7b276027.chunk.css"
+  },
+  {
+    "revision": "c4e087ad91d170f5e2b85a1ad16261c1",
+    "url": "/index.html"
+  }
+];
